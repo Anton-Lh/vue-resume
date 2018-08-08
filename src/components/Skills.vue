128b985cc1ec5eab467d1565ce36ff4ba6a21201
KyrieCao <864655735@qq.com>
2018-08-08 12:01:01 +0800

    resumeVersion1.0

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -3,37 +3,32 @@
 		<div class="row">
 			<div class="col-md-12 describe">
 				<h3>个人技能</h3>
-				<h4 class="sub-title">Code：</h4>
+				<h4 class="sub-title">前端：</h4>
 				<ul>
-					<li>熟练使用 HTML 、DIV + CSS 布局（包括 flex 布局），能写出符合 W3C 标准且富有语义化的结构。能够独立解决大部分浏览器、移动端兼容问题。</li>
-					<li>热衷于研究原生 JavaScript 熟悉 ES5/ES6 ，熟悉其原型、闭包、继承，且了解一些JavaScript的坑点。日常使用 SCSS 、ES5/ES6 进行开发。</li>
-					<li>熟练使用 jQuery ，并阅读过其部分源码，了解其无 new 操作、链式调用、对象扩展等原理。</li>
-					<li>熟悉 Vue 框架了解其双向数据绑定原理，能够熟练使用vue、vue-router、vuex、axios进行页面开发。实习期间，主动推动 Vue 在团队内部的使用。另外，自己用vue + webpack搭建了自己的
-						<a href="http://www.jayzangwill.cn/">个人主页</a>。</li>
-					<li>玩过node + express + mongodb 曾利用node + express + mongodb + 自己的前端技能搭建出
-						<a href="http://www.jayzangwill.cn/bank-card/">银行卡管理系统</a>。</li>
-					<li>玩过微信小程序，在实习期间完成了清华入学助手（可在微信上搜到）小程序的静态页面和交互效果，了解小程序的一些坑点。</li>
+					<li>熟练使用 HTML 、DIV + CSS 布局（包括 flex 布局），能写出符合 W3C 标准且富有语义化的结构。</li>
+					<li>熟练使用Javascript实现页面的交互效果，熟悉其原型、闭包、继承。日常使用 SCSS，了解ES6新特性。</li>
+					<li>熟练使用 jQuery、Bootstrap，并阅读过其部分源码，了解其无 new 操作、对象扩展等原理。</li>
+					<li>熟悉 Vue 框架了解其双向数据绑定原理，能够熟练使用vue、vue-router、vuex、axios进行页面开发。</li>
 					<li>基础知识扎实，熟悉 HTTP 协议、常见状态码、浏览器缓存机制，了解 JavaScript 一些设计模式。</li>
 				</ul>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-md-12 describe">
-				<h4>Tools</h4>
+				<h4>其它</h4>
 				<ul>
-					<li>熟练使用 gulp 、webpack ，并能独立配置一些常用的配置，为了平时个人开发方便，自己为自己定制了
-						<a href="https://github.com/JayZangwill/myTools/tree/master/gulp">gulp</a> 和
-						<a href="https://github.com/JayZangwill/vue-multipage">webpack</a> 的配置。同时能够使用 Git&GitHub 或 SVN 进行团队合作。</li>
-					<li>能够熟练使用 PS 还原设计稿和切图。</li>
-				</ul>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-12 describe">
-				<h4>Design：</h4>
-				<ul>
-					<li>因实习期间和设计师交流频繁，所以在不自觉中培养了自己的审美能力，能够对产品的 UI 与 UE 设计提出自己的想法，细节强迫症和代码洁癖。</li>
-					<li>注重代码的规范性（代码那么乱以后怎么维护），追求简洁且高性能的代码（页面打开那么慢给谁看）。</li>
+					<li>学习过Python、PHP后台开发，现在主打Java后台。</li>
+					<li>熟悉掌握Java语言，有J2EE开发经验，熟悉MVC开发流程，现解面向对象思想。</li>
+					<li>熟悉Spring、SpringMVC、MyBatis等Web开发框架，自己整合过SSM框架
+						<a href="https://github.com/caozongpeng/SSMDemo" target="_black">SSM框架整合。</a>
+					</li>
+					<li>熟悉SpringBoot快速开发框架、Mavan构建项目，自己用SpringBoot+MyBatis+Thymeleaf搭建的
+						<a href="https://github.com/caozongpeng/SpringBootBlog" target="_black">个人博客。</a>
+					</li>
+					<li>熟悉掌握原生Sql语句，熟悉Mysql、Sqlserver数据库。</li>
+					<li>熟悉Linux下开发，会搭建Java开发环境，熟悉Linux常用命令。</li>
+					<li>了解redis缓存数据库，熟悉版本控制git、svn。</li>
+					<li>注重代码的规范性(代码乱后期无法维护)，追求简洁高性能的代码(页面打开慢给谁看)</li>
 				</ul>
 			</div>
 		</div>
--- src/components/Work.vue
+++ src/components/Work.vue
@@ -2,7 +2,7 @@
     <div class="work">
 		<div class="row">
 			<div class="col-md-12 describe">
-				<h3>2017.5 ~ 2017.11 郴州育才信息科技有限公司 - 开发部 - 后端开发（实习生）</h3>
+				<h3>2017.5 ~ 2017.11 郴州育才信息科技有限公司 - 开发部（实习生）</h3>
 				<ul>
 					<li>
 						<strong>项目描述：</strong>
@@ -10,33 +10,28 @@
                     </li>
 					<li>
 						<strong>主要工作：</strong>
-                        <br>
-                        <strong>园长端 App 开发</strong>
-                        <ul>
-                            <li class="list-circle">
-                                负责项目页面的开发
-                            </li>
-                            <li class="list-circle">
-                                html5、css3 实现原型 UI 界面
-                            </li>
-                            <li class="list-circle">
-                                使用  echarts  实现幼儿园数据图表可视化
-                            </li>
-                            <li class="list-circle">
-                                使用  vee-validate  实现前端表单验证
-                            </li>
-                            <li class="list-circle">
-                                利用  ajax  与后端接口交互并在前端渲染，封装常用代码
-                            </li>
-                        </ul>
-                        <strong>JAVA后端接口开发</strong>
-                        <ul>
-                            <li class="list-circle">负责与  App  交互的接口  原生sql  查询语句的编写、MVC  各层  Java  代码的编写。</li>
-                        </ul>
+                        负责项目部分页面的开发，html5、css3 实现原型 UI 界面,利用  ajax  与后端接口交互并在前端渲染，封装常用代码，负责与  App  交互的接口  原生sql  查询语句的编写、MVC  各层  Java  代码的编写。
                     </li>
                     <li>
                         <strong>工作成果:</strong>
-                        熟悉整个软件的开发流程，了解前后端分享架构。
+                        熟悉整个软件的开发流程，了解前后端分层架构。
+                    </li>
+				</ul>
+			</div>
+            <div class="col-md-12 describe">
+				<h3>2018.3 ~ 2018.6 广州店智信息科技有限公司 - 技术部（实习生）</h3>
+				<ul>
+					<li>
+						<strong>项目描述：</strong>
+                        店智兵兵，是国内首个基于O2O的家居门店管理系统（SaaS平台），门店运营必不可少的端口管理、销售单管理、订单地图、库存管理、定货管理、端口展示等等。
+                    </li>
+					<li>
+						<strong>主要工作：</strong>
+                        负责项目的页面开发，功能优化、项目维护、项目测试、以及公司文档整理等工作，项目基于PHP的ThinkPHP框架开发，主要负责后台接口功能实现，以及前台页面重构，项目初期用的JQuery，后用Vue重构前台页面，单页应用提高用户体验。
+                    </li>
+                    <li>
+                        <strong>工作成果:</strong>
+                        对Vue有着更加深入的理解，对客户的需求了解有提升，写文档能力有着不错的提高。
                     </li>
 				</ul>
 			</div>
@@ -47,12 +42,12 @@
 .describe {
     margin-bottom: 25px;
     color: #fff;
-    .list-circle {
-        list-style-type: circle;
+    h3 {
+        text-align: center;
     }
     li {
         font-size: 16px;
-        margin-bottom: 5px;
+        margin-bottom: 10px;
         a {
             &:link,
             &:visited {
