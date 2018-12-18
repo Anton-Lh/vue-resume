1a9ec898761f41d4bc6b4cd259b5ca87e197a60b
kyrie <864655735@qq.com>
2018-12-18 23:36:00 +0800

    update 12-18

--- src/components/Experience.vue
+++ src/components/Experience.vue
@@ -2,11 +2,29 @@
     <div class="experience-content">
 		<h3 class="text-center">公司项目</h3>
 
-		<h4>店智家居门店管理系统</h4>
-		<p><strong>涉及技术：</strong>PHP、ThinkPHP、Vue、weui、JQuery 等。</p>
-		<p><strong>项目描述：</strong>由于项目基于微信公众号里的HTML5页面，初始架构用的Jquery，后面重构用Vue，单页应用提高用户体验。</p>
-		<p><strong>责任描述：</strong>对项目进行维护，页面代码优化。</p>
-		<p><strong>个人收获：</strong>了解了Vue、Weui等多种框架的使用，了解了整个项目的流程和前后端数据的交互方法（ajax、jsonp等），自我学习能力得到提升、解决问题的方法得到丰富。</p>
+		<h4>湖南国税办税服务管理系统</h4>
+		<p><strong>涉及技术：</strong>Spring、SpringMVC、Hibernate、Oracle、Redis、JQuery、Ztree、Ajax等。</p>
+		<p><strong>项目描述：</strong>项目主要有10大模块，分别是系统管理、全局监控、统计分析、预警报警、绩效考核、在线学习、预约管理、涉税咨询、设备管理、后台管理。</p>
+		<p><strong>责任描述：</strong>我在此项目中主要负责全局监控模块以及绩效考核模块的开发，全局监控（包括：网上办税服务厅监控、办税服务厅实时监控、音视频实时查看、音视频回放、虚拟大厅以及电子地图等功能）使用到了第三方海康威视视频监控，使用到WebService调用远程服务，绩效考核（包括：单位考核以及个人考核）在此模块主要难点在于对公式考核的设置上。</p>
+		<p><strong>个人收获：</strong>对使用第三方的API有更加深入的理解。</p>
+
+        <h4>中富电路MES管理系统</h4>
+		<p><strong>涉及技术：</strong>SpringBoot、Shiro、Mybatis-plus、Beet、MySQL、MyCat等。</p>
+		<p><strong>项目描述：</strong>制造执行系统（Manufacturing Execution System，MES）是面向车间的生产过程管理的实时信息系统。它填补了上层生产计划与底层工业控制之间的鸿沟，可以为用户营造一个可视、实时、精细、可追溯的制造环境。MES目的是在收集生产过程中大量的实时数据后，使用实时精确的生产过程数据可以实现。项目主要有5大主要模块，分别为总体设置状况分析、产品追溯、异常管理、预警报警、车间看板等模块，主要功能以生产车间的设备状况分析，如沉铜、DVCP、镀金、夹具、飞针数据分析等，分析主要是通过列表、图表展示等方式。</p>
+		<p><strong>责任描述：</strong>在此项目中我和另一个同事负责设置状况分析模块，由于我是后来加入该项目的，在此我发现生产车间每天都会生产出大量数据，导致一个很严重的问题就是数据量太大对数据库的访问压力非常大，且查询速度非常的慢。因此我在项目中加入Mycat数据库分片中间件进行数据库的优化工作，从而减少数据库的访问压力提高查询速度。</p>
+		<p><strong>个人收获：</strong>对使用第三方的API有更加深入的理解。</p>
+
+        <h4>鹏飞OA自动化办公系统</h4>
+		<p><strong>涉及技术：</strong>SpringBoot、Mybatis、JPA+MySQL、Freemarker、Bootstrap等。</p>
+		<p><strong>项目描述：</strong>鹏飞公司的内部办公系统，面向组织的日常动作和管理，员工及管理者使用频率最高的应用系统，极大提高公司的办公效率。</p>
+		<p><strong>责任描述：</strong>我在此项目中主要负责邮件管理、任务管理以及文件管理模块的开发，邮件管理（包括：写信，收件箱、发件箱等信息查看，写信可以添加附件等功能）任务管理（包括：任务列表，我的任务两个功能，可以添加任务，修改任务状态等功能）文件管理（包括：文件管理，主要上传各类型的文件，文件删除，文件分类等功能）。</p>
+		<p><strong>个人收获：</strong>对使用第三方的API有更加深入的理解。</p>
+
+        <h4>幼儿园管理平台</h4>
+		<p><strong>涉及技术：</strong>Spring、SpringMvc、Mybatis、ApiCloud、Vue等。</p>
+		<p><strong>项目描述：</strong>育才小管家  App  是 使用  apicloud  开发的，分为园长端和家长端，园长端展示幼儿园数据，家长端展示幼儿数据。</p>
+		<p><strong>责任描述：</strong>我在此项目中负责和另一个同事负责移动端App开发和后端接口开发，项目原本是用的JQuery，后来项目中引入了Vue与Axios提升开发效率，用户体验也大大提升，后端部分负责Api接口的编写，并对接登录接口到阿里大于短信平台。</p>
+		<p><strong>个人收获：</strong>对使用第三方的API有更加深入的理解。</p>
 
 		<h3 class="text-center">个人项目</h3>
 
@@ -35,7 +53,8 @@ export default {
     color: #fff;
     p {
         font-size: 16px;
-        text-indent: 2em;
+        text-indent: -5em;
+        margin-left: 7em;
     }
     a {
         &:link,
--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -2,33 +2,19 @@
     <div class="skill">
 		<div class="row">
 			<div class="col-md-12 describe">
-				<h3>个人技能</h3>
-				<h4 class="sub-title">前端：</h4>
+				<h3>技术总结</h3>
+				<!-- <h4 class="sub-title">前端：</h4> -->
 				<ul>
-					<li>熟练使用 HTML 、DIV + CSS 布局（包括 flex 布局），能写出符合 W3C 标准且富有语义化的结构。</li>
-					<li>熟练使用Javascript实现页面的交互效果，熟悉其原型、闭包、继承。日常使用 SCSS，了解ES6新特性。</li>
-					<li>熟练使用 jQuery、Bootstrap，并阅读过其部分源码，了解其无 new 操作、对象扩展等原理。</li>
-					<li>熟悉 Vue 框架了解其双向数据绑定原理，能够熟练使用vue、vue-router、vuex、axios进行页面开发。</li>
-					<li>基础知识扎实，熟悉 HTTP 协议、常见状态码、浏览器缓存机制，了解 JavaScript 一些设计模式。</li>
-				</ul>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-12 describe">
-				<h4>其它</h4>
-				<ul>
-					<li>学习过Python、PHP后台开发，现在主打Java后台。</li>
-					<li>熟悉掌握Java语言，有J2EE开发经验，熟悉MVC开发流程，现解面向对象思想。</li>
-					<li>熟悉Spring、SpringMVC、MyBatis等Web开发框架，自己整合过SSM框架
-						<a href="https://github.com/caozongpeng/SSMDemo" target="_black">SSM框架整合。</a>
-					</li>
-					<li>熟悉SpringBoot快速开发框架、Mavan构建项目，自己用SpringBoot+MyBatis+Thymeleaf搭建的
-						<a href="https://github.com/caozongpeng/SpringBootBlog" target="_black">个人博客。</a>
-					</li>
-					<li>熟悉掌握原生Sql语句，熟悉Mysql、Sqlserver数据库。</li>
-					<li>熟悉Linux下开发，会搭建Java开发环境，熟悉Linux常用命令。</li>
-					<li>了解redis缓存数据库，熟悉版本控制git、svn。</li>
-					<li>注重代码的规范性(代码乱后期无法维护)，追求简洁高性能的代码(页面打开慢给谁看)</li>
+					<li>熟练掌握JavaEE语言以及基于JSP的Web应用程序开发，具有扎实的编程功底。</li>
+					<li>熟悉MVC开发模式，熟练使用Struts2、Hibernate、Spring、SpringMVC、MyBatis以及SpringBoot、SpringData家族等主流框架，掌握框架的原理及其整合应用。</li>
+					<li>熟练使用HTML5、CSS3、JavaScript、 JQuery、Ajax、EasyUI、Bootstrap等Web编程技术。</li>
+					<li>熟练掌握SQL语句，熟悉MySQL、SqlServer了解Oracle以及Redis、Mongodb非关系型数据库。</li>
+					<li>熟悉Linux操作系统，熟练使用Linux相关命令进行项目部署，搭建过Zookeeper、SolrCloud、RedisCluster集群，会使用Docker容器化部署。</li>
+                    <li>熟悉ActiveMQ消息队列，Mycat数据库分片，Solr搜索引擎，以及SpringSecurity、Shiro权限控制。</li>
+                    <li>掌握单例、工厂、代理等设计模式，熟悉GIt、SVN版本控制工具。</li>
+                    <li>了解AngularJs、Vue.js等MVVM前端框架，以及iView、ElementUI。</li>
+                    <li>了解Dubbo+Zookeeper面向服务的SOA分布式框架，掌握Nginx反向代理实现Tomcat负载均衡。</li>
+                    <li>了解TCP/IP，Http等网络协议，Socket网络通信编程。</li>
 				</ul>
 			</div>
 		</div>
@@ -53,6 +39,7 @@ export default {
     li {
         list-style-type: circle;
         font-size: 16px;
+        padding-bottom: 10px;
         a {
             &:link,
             &:visited {
